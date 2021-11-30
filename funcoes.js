50eb0a59dfb39768a9f69916520dc251aa6c28e3
Aluno da Infinity School <aluno@3D.local>
2021-11-29 21:28:36 -0300

    mensagem

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,9 @@
 function mudaFoto(foto) {
-		document.getElementById("tipo_pizza").src = foto;
-	}
+	document.getElementById("tipo_pizza").src = foto;
+}
+function SucoDeLaranja() {
+	document.getElementById("saborPizzaSuco").innerHTML = "Suco de Laranja"
+}
+function semDesc() {
+	document.getElementById("saborPizzaSuco").innerHTML = "Suco de Laranja"
+}
